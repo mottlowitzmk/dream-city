c86cb44e80e040d3f5e0f6f7b5b705b011f59e5e
mottlowitzmk <mottlowitzmk@mx.lakeforest.edu>
2017-02-14 11:12:05 -0600

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -8,12 +8,16 @@ function draw() {
   vertex(599,1)
   vertex(599,599)
   vertex(-1,601)
+  fill(#3F3E31);
   endShape();
+  
+  //
   
   
   beginShape();
   vertex(-311,-315)
-  vertex(599.9,-315)  
+  vertex(599.9,-315) 
+  fill(#3C4739); 
   endShape();  
   
   beginShape();
@@ -23,8 +27,9 @@ function draw() {
   vertex(-91.3,-55.2)
   vertex(-212.6,-39.1)
   vertex(-170.7,-110.7)
-
+fill(#272F25);
   endShape();  
+  //st13
   
   
   beginShape();
@@ -46,7 +51,11 @@ function draw() {
   vertex(210.7,367.5)
   vertex(47.6,379)
   vertex(-29.9,493.1)
+  vertex(-50.8,464.9)
+  vertex(14.8,372.7)
+  fill(#313629);
   endShape();  
+  //st18
   
   
   beginShape();
@@ -55,7 +64,9 @@ function draw() {
   vertex(197.1,594.4)
   vertex(196.2,617.4)
   vertex(149.8,365.9)
-  endShape();  
+  fill(#5E886E);
+  endShape(); 
+  //st25 
   
   beginShape();
  
@@ -63,8 +74,9 @@ function draw() {
   vertex(106.1,358.6)
   vertex(106.1,672.4)
   vertex(85.6,676.3)
-
+fill(#393629);
   endShape();  
+  //st26
   
   beginShape();
  
@@ -72,8 +84,8 @@ function draw() {
   vertex(85.6,365.9)
   vertex(85.6,676.3)
   vertex(43.2,676.3)
-  
-  
+  fill(#0A0A09);
+  //st27
   endShape();  
   
   beginShape();
@@ -85,8 +97,9 @@ function draw() {
   vertex(638.3,300.3)
   vertex(590.8,300.3)
   vertex(328.3,198.7)
-  
+  fill(#6E7C70);
   endShape();  
+  //st28
   
   beginShape();
  
@@ -95,7 +108,8 @@ function draw() {
   vertex(638.3,-40.1)
   vertex(638.3,-119)
   vertex(524.1,-58.3)
-
+fill(#689891);
+//st29
   endShape();  
   
   
@@ -105,7 +119,8 @@ function draw() {
   vertex(543.2,115.7)
   vertex(638.3,152.4)
   vertex(638.3,244.5)
-  
+  fill(#53695D);
+  //st30
   endShape();  
   
   beginShape();
@@ -114,7 +129,8 @@ function draw() {
   vertex(590.8,300.3)
   vertex(610,358.6)
   vertex(561.9,429.7)
-  
+  fill(#547866);
+  //st31
   endShape();  
   
   beginShape();
@@ -124,7 +140,8 @@ function draw() {
   vertex(638.3,-169.7)
   vertex(638.3,-110.7)
   vertex(518.4,-55.7)
-  
+  fill(#69736D);
+  //st32
   endShape();  
   
   beginShape();
@@ -133,7 +150,8 @@ function draw() {
   vertex(-224.4,288.9)
   vertex(-224.4,303)
   vertex(-389,311.8)
-  
+  fill(#327A68);
+  //st35
   endShape();  
   
   beginShape();
@@ -144,7 +162,8 @@ function draw() {
   vertex(-389,288.9)
   vertex(-216.5,288.9)
   vertex(-229.2,106.1)
-  
+  fill(#52806C);
+  //st36
   endShape();  
   
   beginShape();
@@ -157,8 +176,9 @@ function draw() {
   vertex(11.9,623.2)
   vertex(-51.1,627.6)
   vertex(-44,472.6)
-  
+  fill(#557A65);
   endShape();  
+  //st37
   
   
   beginShape();
@@ -168,7 +188,8 @@ function draw() {
   vertex(424.8,58.3)
   vertex(288.6,121.7)
   vertex(265,38.6)
-  
+  fill(#43806D);
+  //st43
   
   endShape();  
   
@@ -179,8 +200,9 @@ function draw() {
   vertex(508.2,17.9)
   vertex(454.9,-8.9)
   vertex(367.8,12.1)
-  
+  fill(#606F64);
   endShape();  
+  //st45
   
   beginShape();
  
@@ -188,7 +210,7 @@ function draw() {
   vertex(502.6,20.6)
   vertex(580.1,-14.2)
   vertex(543.2,-43)
-  
+  fill(#6D8681);
   endShape();  
   
   beginShape();
@@ -197,8 +219,8 @@ function draw() {
   vertex(-259.2,513.6)
   vertex(-263.5,645.8)
   vertex(-341.3,648.2)
-  
-  
+  fill(#556E62);
+  //st47
   endShape();  
   
   beginShape();
@@ -207,7 +229,8 @@ function draw() {
   vertex(-238.9,517.4)
   vertex(-238.9,641.2)
   vertex(-267.7,646.6)
-  
+  fill(#48857A);
+  //st48
   
   endShape();  
   
@@ -217,7 +240,8 @@ function draw() {
   vertex(-224.4,303)
   vertex(-224.4,330.3)
   vertex(-385.1,333.7)
-  
+  fill(#425D49);
+  //st49
   
   endShape();  
   
@@ -227,7 +251,7 @@ function draw() {
   vertex(-224.4,330.3)
   vertex(-228.2,367.5)
   vertex(-381.2,365)
-  
+  fill(#344131);
   endShape();  
   
   
@@ -237,7 +261,8 @@ function draw() {
   vertex(-111.6,198.7)
   vertex(26.2,383.9)
   vertex(6.7,429.7)
-  
+  fill(#363B2B);
+  //st52
   
   endShape();  
   
@@ -247,7 +272,8 @@ function draw() {
   vertex(638.3,-40.1)
   vertex(638.3,89.3)
   vertex(508.2,17.9)
-  
+  fill(#BEBA9E);
+  //st54
   
   endShape();  
   
@@ -259,7 +285,8 @@ function draw() {
   vertex(275,-165.8)
   vertex(189,-192.6)
   vertex(203.1,-221.8)
-  
+  fill(#766F73);
+  //st56
   
   endShape();  
   
@@ -269,7 +296,8 @@ function draw() {
   vertex(-79.1,198.7)
   vertex(31.1,365.9)
   vertex(19.4,379)
-  
+  fill(#35392C);
+  //st57
   endShape();  
   
   beginShape();
@@ -277,7 +305,8 @@ function draw() {
   vertex(328.3,198.7)
   vertex(312.2,333.7)
   vertex(590.8,300.3)
-  
+  fill(#668B79);
+  //st58
   
   endShape();  
   
@@ -287,7 +316,8 @@ function draw() {
   vertex(411.7,321.8)
   vertex(301.3,333.7)
   vertex(301.3,446.3)
-  
+  fill(#15120F);
+  //st61
   
   endShape();  
   
@@ -297,7 +327,8 @@ function draw() {
   vertex(312.2,234.8)
   vertex(296.2,365.9)
   vertex(265,365.9)
-  
+  fill(#3B4238);
+  //st63
   
   endShape();  
   
@@ -307,7 +338,8 @@ function draw() {
   vertex(139.8,359.9)
   vertex(124.8,668.5)
   vertex(106.1,664.1)
-  
+  fill(#384231);
+  //st64
   
   endShape();  
   
@@ -317,9 +349,9 @@ function draw() {
   vertex(154.9,362.5)
   vertex(147.5,668.5)
   vertex(124.8,668.5)
-  
+  fill(#49443A);
   endShape();  
-  
+  //st65
   
   beginShape();
  
@@ -327,8 +359,9 @@ function draw() {
   vertex(-95.6,-257.4)
   vertex(-99.8,-175.5)
   vertex(-137.6,-169.7)
-  
-  endShape();  
+  fill(#1A1911);
+  endShape();
+  //st69  
   
   beginShape();
  
@@ -339,9 +372,19 @@ function draw() {
   vertex(-50.8,-127.8)
   vertex(-87.4,-129.7)
   vertex(-76.9,-257.4)
-  
+  fill(#3A463A);
+  //st70
   
   endShape();  
+  
+  beginShape();
+  vertex(-50.8,-127.8)
+  vertex(-8.9,-119)
+  vertex(18.6,-247.2)
+  vertex(-40.1,-241.8)
+  fill(#427C5F);
+  endShape();
+  //st72
   
   beginShape();
  
@@ -349,8 +392,8 @@ function draw() {
   vertex(47.6,-119)
   vertex(53.1,-257.4)
   vertex(-29.4,-247.2)
-  
-  
+  fill(#678979);
+  //st73
   endShape();  
   
   beginShape();
@@ -359,7 +402,8 @@ function draw() {
   vertex(227.1,-263.5)
   vertex(114.9,-270.1)
   vertex(129.5,-343.2)
-  
+  fill(#788080);
+  //st76
   endShape();  
   
   beginShape();
@@ -369,8 +413,18 @@ function draw() {
   vertex(129.5,-343.2)
   vertex(47.6,-343.2)
   vertex(40.8,-278.8)
-  
+  fill(#5B584E);
+  //st79
   endShape();  
+  
+  beginshape();
+vertex(166,-275)
+vertex(232.6,-278.8)
+vertex(201.9,-192.6)
+vertex(145.4,-192.6)
+//st81
+fill(#655952);
+  endShape();
   
   beginShape();
  
@@ -384,7 +438,8 @@ function draw() {
   vertex(166,-96.6)
   vertex(182.1,-152.4)
   vertex(197.7,-200.9)
-  
+  fill(#3F513A);
+  //st82
   endShape();  
   
   beginShape();
@@ -393,8 +448,9 @@ function draw() {
   vertex(100.3,-11)
   vertex(71,-119)
   vertex(76.9,-278.8)
-  
+  fill(#474E3D);
   endShape();  
+  //st85
   
   beginShape();
  
@@ -402,8 +458,9 @@ function draw() {
   vertex(147,-200.9)
   vertex(125.6,-92.7)
   vertex(82.7,-96.6)
-  
+  fill(#407761);
   endShape();  
+  //st86
   
   beginShape();
  
@@ -411,8 +468,8 @@ function draw() {
   vertex(-72.3,-257.4)
   vertex(-78.1,-127.8)
   vertex(-102.5,-127.8)
-  
-  endShape();  
+  fill(#2B2F22);
+  endShape();  //st88
   
   beginShape();
  
@@ -420,7 +477,7 @@ function draw() {
   vertex(459.9,333.7)
   vertex(526.9,486.5)
   vertex(435.1,611.5)
-
+fill(#5D5E5E); //st89
   endShape();  
   
   beginShape();
@@ -430,16 +487,16 @@ function draw() {
   vertex(565.2,636.4)
   vertex(301.3,664.1)
   vertex(301.3,566.2)
-  
-  endShape();  
+  fill(#4A715C);
+  endShape();  //st90
   
   beginShape();
  
   vertex(356.1,646.6)
   vertex(525.7,326.4)
   vertex(623.7,605.7)
-
-  endShape();  
+fill(#729C81);
+  endShape();  //st91
   
  beginShape();
  
@@ -447,8 +504,8 @@ function draw() {
   vertex(606.1,348.3)
   vertex(638.3,414.4)
   vertex(638.3,664.1)
-  
-  endShape();  
+  fill(#7E695F);
+  endShape();  //st93
   
    beginShape();
  
@@ -458,8 +515,8 @@ function draw() {
   vertex(316.6,34)
   vertex(328.3,67.6)
   vertex(275,75)
-  
-  endShape();  
+  fill(#11100D);
+  endShape();  //st95
   
   
    beginShape();
@@ -468,8 +525,10 @@ function draw() {
   vertex(182.1,358.6)
   vertex(196.2,617.4)
   vertex(221.6,627.6)
-  
+  fill(#747768);
   endShape();  
+  //st98
+  
   
    beginShape();
  
@@ -477,6 +536,8 @@ function draw() {
   vertex(252.3,365.9)
   vertex(252.3,664.1)
   vertex(212.8,643.7)  
+  fill(#B8A588);
+  //st99
   endShape();  
   
    beginShape();
@@ -485,7 +546,8 @@ function draw() {
   vertex(508.2,17.9)
   vertex(638.3,89.6)
   vertex(638.3,152.4)
-  
+  fill(#6A786C);
+  //st100
   endShape();  
   
   
